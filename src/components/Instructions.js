d8248a530e9f2524ff2e0fb45f04d45a5cfb4b55
Jakub Bujak <kuba.bujak0@wp.pl>
2023-11-14 14:22:42 +0100

    feature/recipes - initialized basic recipe data and displayed it

---
+++ src/components/Instructions.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Instructions({ title, steps }) {
+	return (
+		<section className="instructions">
+			<h2>Sposób przygotowania</h2>
+			{steps.map((step, i) => (
+				<p key={i}>{step}</p>
+			))}
+   	</section>
+	)
+}
