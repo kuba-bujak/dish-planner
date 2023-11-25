ce2bb442aa56c173f3b092c146e763a57c9fd01d
Jakub Bujak <kuba.bujak0@wp.pl>
2023-11-25 17:53:19 +0100

    restructured code

--- src/components/ColorOrganizer.js
+++ src/components/ColorOrganizer.js
@@ -1,12 +1,13 @@
 import React from "react";
 import ColorList from "./color_organizer/ColorList";
 import AddColorForm from "./color_organizer/AddColorForm";
+import ColorProvider from "../hooks/ColorProvider";
 
 export default function ColorOrganizer() {
   return (
-    <>
+    <ColorProvider>
       <AddColorForm />
       <ColorList />
-    </>
+    </ColorProvider>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React, { createContext } from "react";
 import ReactDOM from "react-dom/client";
 import ColorOrganizer from "./components/ColorOrganizer";
-import ColorProvider from "./hooks/ColorProvider";
+import Menu from "./components/dishes_list/Menu";
+import recipes from "./data/recipes.json";
 
 export const ColorContext = createContext();
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <ColorProvider>
+  <>
+    <Menu recipes={recipes} />
     <ColorOrganizer />
-  </ColorProvider>
+  </>
 );
