d00265fb00be3bef9cb4ffac224dda0592b2f97f
Jakub Bujak <kuba.bujak0@wp.pl>
2024-01-02 13:31:44 +0100

    Added GraphQL request

--- src/components/data-learning/GitHubUser.js
+++ src/components/data-learning/GitHubUser.js
@@ -2,6 +2,36 @@ import React, { useEffect, useState } from "react";
 import { useFetch, useInput } from "../../hooks/hooks";
 import { RepoMenu } from "./RepoMenu";
 import RepositoryReadme from "./RepositoryReadme";
+import { GraphQLClient } from "graphql-request";
+
+const query = `
+	query findRepos($login:String!) {
+		user(login:$login) {
+			login
+			name
+			location
+			avatar_url: avatarUrl
+			repositories(first:100) {
+				totalCount
+				nodes {
+					name
+				}
+			}
+		}
+	}
+`;
+
+const client = new GraphQLClient("https://api.github.com/graphql", {
+  headers: {
+    Authorization: `Bearer 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`,
+  },
+});
+
+client
+  .request(query, { login: "kuba-bujak" })
+  .then((results) => JSON.stringify(results, null, 2))
+  .then(console.log)
+  .catch(console.error);
 
 const loadJSON = (key) => key && JSON.parse(localStorage.getItem(key));
 const saveJSON = (key, data) => localStorage.setItem(key, JSON.stringify(data));
@@ -66,6 +96,8 @@ function SearchForm({ value, onSearch }) {
   );
 }
 
+function List ()
+
 function Fetch({
   uri,
   renderSuccess,
@@ -79,24 +111,54 @@ function Fetch({
 }
 
 export default function UserManager() {
+  // ----------------- Classic Version ----------------- //
+
+  // const [login, setLogin] = useState("kuba-bujak");
+  // const [repo, setRepo] = useState("My-Portfolio");
+
+  // const handleSearch = (login) => {
+  //   if (login) return setLogin(login);
+  //   setLogin("");
+  //   setRepo("");
+  // };
+
+  // if (!login) return <SearchForm value={login} onSearch={handleSearch} />;
+
+  // return (
+  //   <div>
+  //     <h1>User Manager</h1>
+  //     <SearchForm value={login} onSearch={handleSearch} />
+  //     {login && <GitHubUser login={login} />}
+  //     {login && <UserRepos login={login} repo={repo} onSelect={setRepo} />}
+  //     {login && repo && <RepositoryReadme login={login} repo={repo} />}
+  //   </div>
+  // );
+
+  // ----------------- GraphQL Version ----------------- //
+
   const [login, setLogin] = useState("kuba-bujak");
-  const [repo, setRepo] = useState("My-Portfolio");
+  const [userData, setUserData] = useState();
 
-  const handleSearch = (login) => {
-    if (login) return setLogin(login);
-    setLogin("");
-    setRepo("");
-  };
+  useEffect(() => {
+    client
+      .request(query, { login })
+      .then(({ user }) => user)
+      .then(setUserData)
+      .catch(console.error);
+  }, [client, query, login]);
 
-  if (!login) return <SearchForm value={login} onSearch={handleSearch} />;
+  if (!userData) return <p>Wczytywanie...</p>;
 
   return (
-    <div>
-      <h1>User Manager</h1>
-      <SearchForm value={login} onSearch={handleSearch} />
-      {login && <GitHubUser login={login} />}
-      {login && <UserRepos login={login} repo={repo} onSelect={setRepo} />}
-      {login && repo && <RepositoryReadme login={login} repo={repo} />}
-    </div>
+    <>
+      <SearchForm value={login} onSearch={setLogin} />
+      <UserDetails {...userData} />
+      <p>{userData.repositories.totalCount} - repos</p>
+      {/* TODO: Create List component */}
+      <List
+        data={userData.repositories.nodes}
+        renderItem={(repo) => <span>{repo.name}</span>}
+      />
+    </>
   );
 }
